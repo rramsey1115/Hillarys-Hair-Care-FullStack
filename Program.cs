d28145e31336943fc5b3683b26ef18495660441d
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-20 13:15:25 -0600

    get stylist by id works with eslint warnings?

--- Program.cs
+++ Program.cs
@@ -47,6 +47,65 @@ app.MapGet("/api/stylists", (HillarysHairCareDbContext db) => {
     }).ToList();
 });
 
+
+// Get stylist by id
+app.MapGet("/api/stylists/{id}", (HillarysHairCareDbContext db, int id) => {
+    try
+    {
+        Stylist foundS = db.Stylists
+        .Include(s => s.Appointments).ThenInclude(app => app.Customer)
+        .Include(s => s.Appointments).ThenInclude(app => app.AppointmentServices).ThenInclude(aservices => aservices.Service)
+        .SingleOrDefault(s => s.Id == id);
+
+        if(foundS == null)
+        {
+            return Results.NotFound("No stylist with given id");
+        }
+
+        var result = new StylistDTO
+        {
+            Id = foundS.Id,
+            Name = foundS.Name,
+            Email = foundS.Email,
+            Bio = foundS.Bio,
+            ImgUrl = foundS.ImgUrl,
+            Appointments = foundS.Appointments?.Select(a => new AppointmentDTO
+            {
+                Id = a.Id,
+                StylistId = a.StylistId,
+                CustomerId = a.CustomerId,
+                Customer = new CustomerDTO
+                {
+                    Id = a.Customer.Id,
+                    Name = a.Customer.Name,
+                    Email = a.Customer.Email
+                },
+                Date = a.Date,
+                AppointmentServices = a.AppointmentServices?.Select(asvc => new AppointmentServiceDTO
+                {
+                    Id = asvc.Id,
+                    AppointmentId = asvc.AppointmentId,
+                    ServiceId = asvc.ServiceId,
+                    Service = new ServiceDTO
+                    {
+                        Id = asvc.Service.Id,
+                        Name = asvc.Service.Name,
+                        Price = asvc.Service.Price
+                    }
+                }).ToList()
+            }).ToList()
+        };
+#pragma warning restore CS8601 // Possible null reference assignment.
+        return Results.Ok(result);
+    }
+
+    catch (Exception ex)
+    {
+        return Results.NotFound(ex);
+    }
+});
+
+
 // GET all Customers
 app.MapGet("/api/customers", (HillarysHairCareDbContext db) => {
     return db.Customers.OrderBy(c => c.Id).Select(c => new CustomerDTO
