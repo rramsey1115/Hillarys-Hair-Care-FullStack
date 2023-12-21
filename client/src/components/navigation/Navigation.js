8616007fed3d712d1a23a1ae44fe60e322390086
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-21 11:20:37 -0600

    styling

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -3,19 +3,25 @@ import logo from "../../logo/logo.png"
 
 export const Navigation = () => {
     return (
-        <Navbar color="black" dark expand="sm">
-            <Nav navbar style={{alignItems:"center"}}>
+        <Navbar color="black" dark expand="sm" style={{alignItems:"center", padding:2, margin:0}}>
+            <Nav navbar style={{alignItems:"center", padding:0, margin:0}}>
                 <NavbarBrand href="/">
                     <img style={{width:125, border:"solid", borderColor:"white"}} id="nav-logo" src={logo} alt="logo"/>
                 </NavbarBrand>
                 <NavItem style={{marginLeft:20}}>
-                    <NavLink href="/customers"><h5>Customers</h5></NavLink>
+                    <NavLink href="/customers">
+                        <h5 style={{margin:0}}>Customers</h5>
+                    </NavLink>
                 </NavItem>
                 <NavItem style={{marginLeft:30}}>
-                    <NavLink href="/stylists"><h5>Stylists</h5></NavLink>
+                    <NavLink href="/stylists">
+                        <h5 style={{margin:0}}>Stylists</h5>
+                    </NavLink>
                 </NavItem>
                 <NavItem style={{marginLeft:30}}>
-                    <NavLink href="/appointments"><h5>Appointments</h5></NavLink>
+                    <NavLink href="/appointments">
+                        <h5 style={{margin:0}}>Appointments</h5>
+                    </NavLink>
                 </NavItem>
             </Nav>
         </Navbar>
