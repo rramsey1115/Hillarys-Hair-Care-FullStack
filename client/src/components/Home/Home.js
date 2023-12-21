1ff76eb5ef6e730451446f9494d2ff18d8061f22
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-21 12:25:17 -0600

    added Home draft

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,35 @@ export const Home = () => {
             <h1>Home</h1>
         </div>
         <div className="main">
-
+            <h4>About</h4>
+            <p>
+                This application was built as an exercise for NSS. "Hillary" hired me to build an application to help her schedule appointments between customers and stylists.
+                I was given a list of needs from Hillary, and created a wireframe, ERD, GitHub tickets, GitHub project board, and repositroy for this project.
+            </p>
+            <h4>Technical Details</h4>
+            <p>
+                This app was built with a ASP/.NET Minimal API web api, using PostgreSQL and Entity Framework Core for data storage and access.
+                The front-end was built using React, Bootstrap, and vanilla CSS.
+            </p>
+            <h4>Skills Used</h4>
+            <ul>
+                <li>C#/.NET</li>
+                <li>React</li>
+                <li>PostgreSQL</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+                <li>pgAdmin</li>
+                <li>VSCode</li>
+                <li>Swagger</li>
+                <li>Entity Framework Core</li>
+                <li>Web API</li>
+                <li>GitHub</li>
+                <li>Project Planning</li>
+                <li>Ticket Board</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
     </div>)
 }
