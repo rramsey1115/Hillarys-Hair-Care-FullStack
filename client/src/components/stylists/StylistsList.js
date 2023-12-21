7ef1d1560ec22a88a8981baaebc696dfed5e8743
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-21 15:51:45 -0600

    stylist list populated

--- client/src/components/stylists/StylistsList.js
+++ client/src/components/stylists/StylistsList.js
@@ -1,10 +1,28 @@
-import { Table } from "reactstrap"
+import { useEffect, useState } from "react";
+import { Button, Table } from "reactstrap"
+import { getAllStylists } from "../../data/StylistsData";
+import { useNavigate } from "react-router-dom";
 
 export const StylistList = () => {
+    const [stylists, setStylists] = useState([]);
+
+    useEffect(() => {getAndSetStylists()}, []);
+
+    const getAndSetStylists = () => {
+        getAllStylists().then(data => setStylists(data));
+    }
+
+    const navigate = useNavigate();
+
     return (
     <div className="container">
         <div className="header">
             <h1>Stylists</h1>
+            <Button
+                className="header-button"
+                size="md"
+                >New +
+            </Button>
         </div>
         <div className="main">
             <Table>
@@ -13,11 +31,30 @@ export const StylistList = () => {
                         <th>Id</th>
                         <th>Name</th>
                         <th>Email</th>
+                        <th>Active</th>
                         <th>Details</th>
-                        <th></th>
-                        <th></th>
                     </tr>
                 </thead>
+                <tbody>
+                    {stylists?.map((s) => {
+                        return(
+                        <tr key={s.id}>
+                            <th scope="row">{s.id}</th>
+                            <td>{s.name}</td>
+                            <td>{s.email}</td>
+                            <td>{s.isActive ? "Active" : "Inactive"}</td>
+                            <td>
+                                <Button 
+                                value={s.id} 
+                                onClick={e => navigate(`${e.target.value}`)} 
+                                size="sm" 
+                                color="secondary"
+                                >Details
+                                </Button>
+                            </td>
+                        </tr>)
+                    })}
+                </tbody>
             </Table>
         </div>
     </div>)
