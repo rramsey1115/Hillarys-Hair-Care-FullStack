1555dfebf3bc062fde32f8fc7f5174ffcc161306
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-21 11:55:59 -0600

    navbar moved to side

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,3 +1,3 @@
 export const Home = () => {
-    return <h1>HOME</h1>
+    return <div className="container"><h1>HOME</h1></div>
 }
--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,29 +1,32 @@
 import {Nav, NavItem, Navbar, NavbarBrand, NavLink } from "reactstrap"
 import logo from "../../logo/logo.png"
+import "./Navigation.css"
 
 export const Navigation = () => {
     return (
-        <Navbar color="black" dark expand="sm" style={{alignItems:"center", padding:2, margin:0}}>
-            <Nav navbar style={{alignItems:"center", padding:0, margin:0}}>
+    <div className="nav-container">
+        <Navbar color="none" dark expand="sm">
+            <Nav navbar vertical>
                 <NavbarBrand href="/">
                     <img style={{width:125, border:"solid", borderColor:"white"}} id="nav-logo" src={logo} alt="logo"/>
                 </NavbarBrand>
-                <NavItem style={{marginLeft:20}}>
+                <NavItem>
                     <NavLink href="/customers">
                         <h5 style={{margin:0}}>Customers</h5>
                     </NavLink>
                 </NavItem>
-                <NavItem style={{marginLeft:30}}>
+                <NavItem>
                     <NavLink href="/stylists">
                         <h5 style={{margin:0}}>Stylists</h5>
                     </NavLink>
                 </NavItem>
-                <NavItem style={{marginLeft:30}}>
+                <NavItem>
                     <NavLink href="/appointments">
                         <h5 style={{margin:0}}>Appointments</h5>
                     </NavLink>
                 </NavItem>
             </Nav>
         </Navbar>
+    </div>
     )
 }
