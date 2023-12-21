88a680351ea0051e2ef81e99b28102e592040771
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-21 11:59:21 -0600

    navbar styling

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -8,21 +8,21 @@ export const Navigation = () => {
         <Navbar color="none" dark expand="sm">
             <Nav navbar vertical>
                 <NavbarBrand href="/">
-                    <img style={{width:125, border:"solid", borderColor:"white"}} id="nav-logo" src={logo} alt="logo"/>
+                    <img style={{width:180, border:"solid", borderColor:"white"}} id="nav-logo" src={logo} alt="logo"/>
                 </NavbarBrand>
                 <NavItem>
                     <NavLink href="/customers">
-                        <h5 style={{margin:0}}>Customers</h5>
+                        <h4 style={{marginTop:10}}>Customers</h4>
                     </NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink href="/stylists">
-                        <h5 style={{margin:0}}>Stylists</h5>
+                        <h4 style={{marginTop:10}}>Stylists</h4>
                     </NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink href="/appointments">
-                        <h5 style={{margin:0}}>Appointments</h5>
+                        <h4 style={{marginTop:10}}>Appointments</h4>
                     </NavLink>
                 </NavItem>
             </Nav>
