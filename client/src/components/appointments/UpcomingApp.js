eb8ff35fc3fde0954aa61c7c9bb5e550521a1ec6
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-21 16:23:18 -0600

    table and components setup

---
+++ client/src/components/appointments/UpcomingApp.js
@@ -0,0 +1,21 @@
+import { Table } from "reactstrap"
+
+export const UpcomingApp = () => {
+    return (
+        <Table>
+            <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Customer</th>
+                        <th>Stylist</th>
+                        <th>Date</th>
+                        <th>Services</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+        </Table>
+    )
+}
